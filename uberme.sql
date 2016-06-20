5189820b4aa244248a2825497389eede793bf351
fathitarek <ftarek@fcih1.com>
2016-06-20 02:53:59 +0200

    updated database

--- uberme.sql
+++ uberme.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 31, 2016 at 11:55 AM
+-- Generation Time: Jun 20, 2016 at 03:48 AM
 -- Server version: 5.6.30-0ubuntu0.15.10.1
 -- PHP Version: 5.6.11-1ubuntu3.4
 
@@ -29,7 +29,16 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `invitations` (
   `invitation_id` int(11) NOT NULL,
   `invitation_content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invitations`
+--
+
+INSERT INTO `invitations` (`invitation_id`, `invitation_content`) VALUES
+(1, 'hello'),
+(2, 'hello2'),
+(3, 'hello3');
 
 -- --------------------------------------------------------
 
@@ -38,20 +47,19 @@ CREATE TABLE IF NOT EXISTS `invitations` (
 --
 
 CREATE TABLE IF NOT EXISTS `members` (
-  `user_id` int(11) NOT NULL,
+  `uuid` varchar(100) NOT NULL,
   `name` varchar(30) NOT NULL,
-  `phone_number` int(11) NOT NULL,
   `email` varchar(30) NOT NULL,
   `status` tinyint(4) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `members`
 --
 
-INSERT INTO `members` (`user_id`, `name`, `phone_number`, `email`, `status`) VALUES
-(1, 'fathi', 1116763795, 'ftarek@fcih1.com', 1),
-(2, 'noran', 1065722222, 'noran@noran.com', 0);
+INSERT INTO `members` (`uuid`, `name`, `email`, `status`) VALUES
+('324-4tfdcx-44', 'noran', 'noran@yahoo.com', 1),
+('geg-5444-343d', 'fathi', 'ftarek@fcih1.com', 1);
 
 -- --------------------------------------------------------
 
@@ -91,11 +99,21 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 --
 
 CREATE TABLE IF NOT EXISTS `trip` (
-  `trip_id` int(11) NOT NULL,
+  `product_id` varchar(100) NOT NULL,
   `pickup_location` varchar(50) NOT NULL,
   `destnation_location` varchar(50) NOT NULL,
-  `trip_time` datetime NOT NULL
+  `trip_time` varchar(20) NOT NULL,
+  `start_time` int(11) NOT NULL,
+  `ennd_time` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `trip`
+--
+
+INSERT INTO `trip` (`product_id`, `pickup_location`, `destnation_location`, `trip_time`, `start_time`, `ennd_time`) VALUES
+('1', 'cairo', 'alex', '98', 98, 7777),
+('2', 'fe', 'la', '3e', 2, 43);
 
 -- --------------------------------------------------------
 
@@ -111,14 +129,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@admin.com', '$2y$10$VMGdIy8E7Xw6cerCVxEwB.KHPsp02rBAPcmpZv/njWRhGhEM8RSeG', 'nbQ3TqpIzDwJBKRkaxXG4hpucOkeY1vd9z8Jt77KSAQVtpDLhopwsSqfwvBx', '2016-05-30 16:45:05', '2016-05-31 06:46:14');
+(1, 'admin', 'admin@admin.com', '$2y$10$VMGdIy8E7Xw6cerCVxEwB.KHPsp02rBAPcmpZv/njWRhGhEM8RSeG', 'RIML1kyPEl4pesWoABbX6VfQMdrsstXyDrGmSMonfSyW2HfMf2WIyIVqZhDk', '2016-05-30 16:45:05', '2016-06-19 16:15:15');
 
 -- --------------------------------------------------------
 
@@ -127,10 +145,20 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `creat
 --
 
 CREATE TABLE IF NOT EXISTS `user_invitation_trip` (
+  `user_invitation_trip_id` int(11) NOT NULL,
   `invitation_id` int(11) NOT NULL,
-  `trip_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `product_id` varchar(100) NOT NULL,
+  `uuid` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_invitation_trip`
+--
+
+INSERT INTO `user_invitation_trip` (`user_invitation_trip_id`, `invitation_id`, `product_id`, `uuid`) VALUES
+(1, 1, '1', '324-4tfdcx-44'),
+(2, 2, '1', '324-4tfdcx-44'),
+(3, 3, '2', 'geg-5444-343d');
 
 --
 -- Indexes for dumped tables
@@ -146,7 +174,7 @@ ALTER TABLE `invitations`
 -- Indexes for table `members`
 --
 ALTER TABLE `members`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`uuid`);
 
 --
 -- Indexes for table `password_resets`
@@ -159,7 +187,7 @@ ALTER TABLE `password_resets`
 -- Indexes for table `trip`
 --
 ALTER TABLE `trip`
-  ADD PRIMARY KEY (`trip_id`);
+  ADD PRIMARY KEY (`product_id`);
 
 --
 -- Indexes for table `users`
@@ -172,10 +200,10 @@ ALTER TABLE `users`
 -- Indexes for table `user_invitation_trip`
 --
 ALTER TABLE `user_invitation_trip`
-  ADD PRIMARY KEY (`invitation_id`,`trip_id`,`user_id`),
-  ADD KEY `invitation_id` (`invitation_id`,`trip_id`,`user_id`),
-  ADD KEY `trip_id` (`trip_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD PRIMARY KEY (`user_invitation_trip_id`),
+  ADD KEY `invitation_id` (`invitation_id`),
+  ADD KEY `uuid` (`uuid`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -185,22 +213,7 @@ ALTER TABLE `user_invitation_trip`
 -- AUTO_INCREMENT for table `invitations`
 --
 ALTER TABLE `invitations`
-  MODIFY `invitation_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `members`
---
-ALTER TABLE `members`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `trip`
---
-ALTER TABLE `trip`
-  MODIFY `trip_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `invitation_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
@@ -210,8 +223,8 @@ ALTER TABLE `users`
 --
 ALTER TABLE `user_invitation_trip`
   ADD CONSTRAINT `user_invitation_trip_ibfk_1` FOREIGN KEY (`invitation_id`) REFERENCES `invitations` (`invitation_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_invitation_trip_ibfk_2` FOREIGN KEY (`trip_id`) REFERENCES `trip` (`trip_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_invitation_trip_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `members` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_invitation_trip_ibfk_2` FOREIGN KEY (`uuid`) REFERENCES `members` (`uuid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_invitation_trip_ibfk_3` FOREIGN KEY (`product_id`) REFERENCES `trip` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
